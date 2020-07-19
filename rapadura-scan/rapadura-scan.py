c329ed3e76079b44c74b4e8bfd08c7da4346cf97
Davison Silva <47065921+davisonv@users.noreply.github.com>
2020-07-19 14:31:26 -0300

    Add files via upload

--- rapadura-scan/rapadura-scan.py
+++ rapadura-scan/rapadura-scan.py
@@ -1,1 +1,28 @@
+# Um portscan simples consumindo a api do Shodan
+import socket
+import shodan
 
+#tratar o alvo
+host = input("Digite a url do alvo:")
+ip = socket.gethostbyname(host)
+
+SHODAN_API_KEY = "VEwqH1TXvRWKxeQ1eAmNoE1zvNLb3iMo"
+
+api = shodan.Shodan(SHODAN_API_KEY)
+
+# analisa o host
+host = api.host(ip)
+
+# imprime as informações gerais
+print("""
+        IP: {}
+        Organization: {}
+        Operating System: {}
+""".format(host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a')))
+
+#imprime os banners
+for item in host['data']:
+        print("""
+                Port: {}
+                Banner: {}
+        """.format(item['port'], item['data']))
